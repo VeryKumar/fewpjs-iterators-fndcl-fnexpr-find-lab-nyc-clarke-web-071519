176987edcf0e189b51dbb604a9108374d2f1f93b
Varun Kumar <varunkumaranalysis@gmail.com>
2019-10-08 10:13:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
-}
+// function testFunc() {
+//   return "hi"
+// }
+
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" }
+  //...
+];
+
+const superbowlWin = arrayOfObjects => {
+  //use find to return the first element which returns true
+  //use the object to find the the year (via find), and then return it
+  //spec case - if nothing is found,return undefined
+  let result = arrayOfObjects.find(
+    arrayOfObjects => arrayOfObjects.result === "W"
+  );
+  return !!result ? result.year : undefined;
+};
